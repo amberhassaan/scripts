b994a7cbd9a1534c4ecb5132d08351304cf8058f
Muhammad Amber Hassaan <amber_474@yahoo.com>
2012-01-29 19:28:57 -0600

    userMem provides a sorted summary of memory usage by a process improvements to footprint

--- footprint.sh
+++ footprint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# sums the amount of memory taken by multiple instances of a process
+# e.g. run footprint.sh 'chrom*'
+# pattern provided as command line parameter
+# matching of pattern is case insensitive
+
+
 MEM_PCNT=4
 RSS=6
 NAME=11
 
-ps aux | awk "{ if (\$$NAME ~ /$1/) { print \$0 }}" | \
-  awk "{ mem+=\$$MEM_PCNT; rss+=\$$RSS; } END{ printf \"$1 memory usage=%g%%, rss=%gMB\\n\", mem, rss/1024 }"
+echo -n "$1, "
+
+ps aux | awk "{ print \$$MEM_PCNT, \$$RSS, \$$NAME; }" | grep -i "$1" | \
+  awk '{ mem+=$1; rss+=$2; } END{ printf "memory usage=%2.3f%%, rss=%2.3f MB\n", mem, (rss/1024)}'
---
+++ userMem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# sums and sorts the total amount of memory taken by user processes as reported by 'ps'
+# Useful to find the processes with most memory usage
+
+RSS=6
+NAME=11
+ps aux | awk "{ sum += \$$RSS; print\$$RSS,\$$NAME} END { print sum, \"Total_user_space_in_use\" }" | sort -k 2 \
+  | awk 'BEGIN {prev="";} { array[$2] += $1; if (prev != "" && prev != $2) { print array[prev], prev; } prev = $2; }' \
+  | sort -k 1 -nr  | awk '($1 > 1000) { if (NR==1) { tot=$1; }; printf "%2.3f MB, %2.3f%%, %s\n", ($1/1024), 100*$1/tot, $2; }'
