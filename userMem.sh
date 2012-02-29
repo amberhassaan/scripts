6654991761ce14358ed6dbb2d16604dd4ac03e56
Muhammad Amber Hassaan <ahassaan@charpak.ices.utexas.edu>
2012-02-29 16:38:16 -0600

    userMem can take a cutoff limit from the command line prints running sum as well

--- userMem.sh
+++ userMem.sh
@@ -3,10 +3,25 @@
 # sums and sorts the total amount of memory taken by user processes as reported by 'ps'
 # Also sums memory usage of multiple instances of same process
 # Useful to find the processes with most memory usage
+# a cutoff limit can be provided at the command line
+# e.g. userMem.sh 0.5
 
+
+CUTOFF=${1:-"0.5"}
 
 RSS=6
 NAME=11
 ps aux | awk "{ sum += \$$RSS; print\$$RSS,\$$NAME} END { print sum, \"Total_user_space_in_use\" }" | sort -k 2 \
   | awk 'BEGIN {prev="";} { array[$2] += $1; if (prev != "" && prev != $2) { print array[prev], prev; } prev = $2; }' \
-  | sort -k 1 -nr  | awk '($1 > 1000) { if (NR==1) { tot=$1; }; printf "%2.3f MB, %2.3f%%, %s\n", ($1/1024), 100*$1/tot, $2; }'
+  | sort -k 1 -nr  | awk \
+  "BEGIN { \
+    cutoff=$CUTOFF; \
+    printf \"INFO: Showing user space processes using >= %0.1f%% of RAM\\n\", cutoff; \
+  } \
+  { \
+    if (NR==1) { tot=\$1; }; \
+    pct=100*\$1/tot; sum += pct; \
+    if (pct >= cutoff) { \
+      printf \"%-60s, %4.3f MB, %2.3f%%, running_sum=%2.3f%%\\n\", \$2, (\$1/1024), pct, (sum-100); \
+    } \
+  }"
