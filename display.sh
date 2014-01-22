4999a05b0353769a77fe8583eb86916f07fc94a7
M. Amber Hassaan <amber_474@yahoo.com>
2014-01-21 22:29:01 -0600

    fix: turn off disconnected outputs

--- display.sh
+++ display.sh
@@ -4,27 +4,29 @@ internal_pattern='LVDS.*'
 internal=$(xrandr | grep ' connected '| awk '{ print $1; }' | grep  "$internal_pattern" | sort| head -n 1)
 external=$(xrandr | grep ' connected '| awk '{ print $1; }' | grep -v "$internal_pattern" | sort | head -n 1)
 
-echo "Found output: $internal";
-echo "Found output: $external";
+echo "Found internal: $internal";
+echo "Found external: $external";
 
 
 mode=${1-"single"}
 position=${2-"--left-of"}; # replace with "--right-of" or "--above" or "--below"
 
-if [ "X$external" != "X" ]; then
-  if [ "$mode" != "single" ] ; then
+if [ "$mode" != "single" ] ; then
+  if [ "X$external" != "X" ]; then
     cmd="xrandr --output $external --auto $position $internal"
     echo "Executing: $cmd";
-    exec $cmd;
+    eval $cmd;
   else
-    cmd="xrandr --output $external --off"
-    echo "Executing: $cmd"
-    exec $cmd;
-  fi;
-else
-  if [ "$mode" != "single" ]; then
     echo "Could not find an external output";
   fi;
+else
+  outputs=$(xrandr | grep ' disconnected ' | awk '{ print $1; }' | tr '\n' ' ');
+  echo $outputs;
+  for o in $outputs; do 
+    cmd="xrandr --output $o --off"
+    echo "Executing: $cmd"
+    eval $cmd;
+  done;
 fi;
 
 
