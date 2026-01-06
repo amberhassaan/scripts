cbba6808cf6a906407111bc1f88d13aeeb744d3d
M. Amber Hassaan <amber_474@yahoo.com>
2026-01-06 08:35:53 -0500

    add opt to possible cuda install paths

--- add-cuda-to-path.sh
+++ add-cuda-to-path.sh
@@ -5,5 +5,8 @@ if [[ -z $(which nvcc) ]] ; then
 
   if [[ -d /usr/local/cuda ]] ; then 
     export PATH="$PATH:/usr/local/cuda/bin"
+  elif [[ -d /opt/cuda ]] ; then
+    export PATH="$PATH:/opt/cuda/bin"
   fi;
+  
 fi;
